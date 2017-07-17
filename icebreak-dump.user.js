34162221c2348ee406b79ced76ddd915999c7295
Andreas Louv <andreas@louv.dk>
2017-07-17 21:34:29 +0200

    Jump between errors

--- icebreak-dump.user.js
+++ icebreak-dump.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Better IceBreak dump
 // @namespace   https://github.com/andlrc/userscripts
-// @version     0.0.2
+// @version     0.0.3
 // @description Better IceBreak dump
 // @match       *://*.admin.workmule.dk/*
 // @match       *://dksrv206:*/*
@@ -12,8 +12,16 @@
 (function() {
 	"use strict";
 
+    if (typeof gotoError === 'undefined')
+        return;
+
+    var lineEls = [];
     [].forEach.call(document.querySelectorAll('span.error'), (errEl) => {
-        var [_, severity, lineNo] = errEl.innerText.split(/\s+/);
+        var columns = errEl.innerText.match(/\*RNF\d+\s+(\d+)\s+(\d+)\s+([^]*)/);
+        if (!columns)
+            return;
+
+        var [_, severity, lineNo, errMsg] = columns;
         if (severity < 20)
             errEl.className = 'warning';
 
@@ -23,15 +31,31 @@
         if (lineEl) {
             lineEl.className = errEl.className;
             lineEl.style.cursor = 'pointer';
-            lineEl.addEventListener('click', gotoError);
-        }
-        errEl.parentNode.addEventListener('click', function(evt) {
-            evt.preventDefault();
+            lineEl.addEventListener('click', cnext.bind(null, lineEl));
+            lineEl.setAttribute('title', errMsg.trim().replace(/\s+/g, ' '));
 
-            if (lineEl)
-                window.scrollTo(0, document.body.scrollTop + lineEl.getBoundingClientRect().top);
-        });
+            errEl.parentNode.addEventListener('click', function(evt) {
+                evt.preventDefault();
+
+                zt(lineEl);
+            });
+
+
+            lineEls.push(lineEl);
+        }
     });
+
+    function cnext(el) {
+        var nextIx = lineEls.indexOf(el) + 1;
+        if (nextIx == lineEls.length)
+            gotoError();
+        else
+            zt(lineEls[nextIx]);
+    }
+
+    function zt(el) {
+        window.scrollTo(0, el.offsetTop);
+    }
 
     var btn = document.createElement('button');
     btn.innerText = 'GOTO Error';

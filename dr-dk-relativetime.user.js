0f57b631c66d02bf6b9a93b094644295b1c6fe95
Andreas Louv <andreas@louv.dk>
2017-08-11 19:40:54 +0200

    Show whole description as a ``title'' attribute.

--- dr-dk-relativetime.user.js
+++ dr-dk-relativetime.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Relative Time in DR.DK live tv
 // @namespace   https://github.com/andlrc/userscripts
-// @version     0.0.8
+// @version     0.0.9
 // @description Show relative time instead of absolute time on dr.dk/tv/live
 // @match       *://dr.dk/tv/live*
 // @match       *://www.dr.dk/tv/live*
@@ -10,30 +10,24 @@
 // @updateURL   https://github.com/andlrc/userscripts/raw/master/dr-dk-relativetime.user.js
 // ==/UserScript==
 (function() {
+	var UPD_TIME = 30 * 1000;
 	"use strict";
 
 	var timeObjects = {};
 
-	// Load moment.js unless dr.dk decides to load it them self
-	if (typeof moment == 'undefined') {
-		var m = document.createElement('script');
-		m.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/locale/da.js";
-		document.head.appendChild(m);
-		var timer_afterLoad = setTimeout(afterLoad, 100);
-	}
-	else {
-		afterLoad();
-	}
-
-	function afterLoad() {
-		if (typeof moment != 'undefined') {
+	var m = document.createElement('script');
+	m.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/locale/da.js";
+	document.head.appendChild(m);
+	var timer_afterLoad = setInterval(function() {
+		if (typeof moment != 'undefined' &&
+		    document.querySelector('.modal-description') != null) {
 			clearTimeout(timer_afterLoad);
-
-			setInterval(updateTime, 30000);
-			updateTime();
+			setInterval(updateLoop, UPD_TIME);
+			updateLoop();
 		}
-	}
-	function updateTime(el) {
+	}, 100);
+
+	function updateLoop(el) {
 		var times = [].slice.call(document.querySelectorAll('time'));
 		times.forEach(function(el) {
 			if (!el.dataset.stored) {
@@ -43,13 +37,18 @@
 			var time = el.getAttribute('title');
 			var date = timeObjects[time] || (timeObjects[time] = moment(time, 'HH:mm'));
 
-			// The date can be above 24 and and on the next day, moment interpretent
-			// this as -x hours instead of +x, assume no program is send after 5am
-			// next day
+			/* The date can be above 24 and and on the next day, moment *
+			 * interpretent this as -x hours instead of +x, assume no program
+			 * is * send after 5am next day */
 			if (date.hours() < 5) {
 				date.add(24, 'hours');
 			}
 			el.innerHTML = date.fromNow();
 		});
+
+		/* Show full description */
+		var descs = [].slice.call(document.querySelectorAll('.modal-description'));
+		descs.forEach(el => el.setAttribute('title', el.innerText));
 	}
 })();
+
